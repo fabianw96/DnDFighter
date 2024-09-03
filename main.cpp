b85c4d2823b93569bf553ec894ad16209aae291e
Fabian Wittgen <wittgenfabian@gmail.com>
2024-09-03 16:38:42 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    //Print intro text
+    //Let Player create a character
+        //Player can choose a class and sepnd points on stats.
+        //Player gets a weapon to start with depending on class
+    //Monsters get created in factory
+        //at first only create 2-3
+            //get stats for monster from Bestiary.csv
+    //Player can choose what monster to fight
+    //Fight starts
+        //Player can choose to either attack or use their ability
+        //Every attack the player AND the monster Roll a 1 D20 + their STR stats to determine if the attack hits
+            //If the value is higher than the enemies AC the attack hits
+        //If the attack hits, damage is calculated based on the equipped weapon + their stats
+        //The damage is then subtracted from the HP of the character that got hit
+}
